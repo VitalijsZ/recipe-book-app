2322481ad9d4522fcc3f566ccd38691c002fedc3
VitalijsZ <vitalijszaharevskis@gmail.com>
2026-04-28 15:12:37 +0300

    Add recipe admin inlines for ingredients and steps

--- Recipe book/recipes/admin.py
+++ Recipe book/recipes/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import Category, Recipe, RecipeCategory, Ingredient, Step
 
-# Register your models here.
+
+class IngredientInline(admin.TabularInline):
+    model = Ingredient
+    extra = 1
+
+
+class StepInline(admin.TabularInline):
+    model = Step
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [IngredientInline, StepInline]
+
+
 admin.site.register(Category)
-admin.site.register(Recipe)
 admin.site.register(RecipeCategory)
 admin.site.register(Ingredient)
 admin.site.register(Step)
